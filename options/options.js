d44ce1868442eaab1ce6c10d629dfc12e4a20a07
Juan B <juan@crowdbotics.com>
2023-05-04 11:26:07 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "juanconnectorsbaseapi-1",
   url: "https://juanconnectorsbaseapi-1.botics.co",
   api: "https://juanconnectorsbaseapi-1.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
